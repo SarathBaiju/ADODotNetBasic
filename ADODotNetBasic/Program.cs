b50fe60e0b1614d507cc9c153aa26f0b1ac7360b
sarath baiju <sarath.bu@frontiersin.net>
2022-01-21 23:09:52 +0530

    Add project files.

---
+++ ADODotNetBasic/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace ADODotNetBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var sqlConnection = new SqlConnection("Data Source=(localdb)\\mssqllocaldb; Initial Catalog=Learning_db");
+
+                sqlConnection.Open();
+
+                var sqlCommand = new SqlCommand("select top 10 * from Employee", sqlConnection);
+
+                var dataTable = new DataTable();
+                var sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                sqlDataAdapter.Fill(dataTable);
+
+                var employees = dataTable.AsEnumerable().Select(dataRow => {
+                    return new Employee
+                    {
+                        Id = dataRow.Field<int>("Id"),
+                        Name = dataRow.Field<string>("Name"),
+                        Age = dataRow.Field<int>("Age")
+                    };
+                }).ToList();
+                sqlConnection.Close();
+
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine(employee.Id);
+                    Console.WriteLine(employee.Name);
+                    Console.WriteLine(employee.Age);
+
+                    Console.WriteLine("----------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
